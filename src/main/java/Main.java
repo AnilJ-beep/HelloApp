6776e5b5c888ec50ca6183a83a9fd9308b5f76fd
AnilJ-beep <aniljampana12345@gmail.com>
2026-04-08 22:06:27 +0530

    UC5 implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+        if (args.length == 0) {
+            System.out.println("Hello World!");
         } else {
-            System.out.println("Hello World");
+            System.out.print("Hello ");
+
+            for (String name : args) {
+                System.out.print(name + " ");
+            }
         }
 
     }
