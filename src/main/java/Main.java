6cad6a2469bfc7c4a48610a2f973096d6db1aa66
AnilJ-beep <aniljampana12345@gmail.com>
2026-04-08 21:59:01 +0530

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length > 0) {
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
             System.out.print("Hello ");
             
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i] + " ");
+            for (String name : args) {
+                System.out.print(name + " ");
             }
-
-            System.out.println(); // move to next line
-        } else {
-            System.out.println("Hello World");
         }
 
     }
