040765bcb81f9cb9c6eda60521d489c7d3a748f8
AnilJ-beep <aniljampana12345@gmail.com>
2026-04-08 23:50:38 +0530

    UC7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-<<<<<<< HEAD
-            System.out.println("Hello World");
-        } else {
-            System.out.print("Hello ");
-            
-            for (String name : args) {
-                System.out.print(name + " ");
-            }
-=======
             System.out.println("Hello World!");
         } else {
-<<<<<<< HEAD
-            System.out.print("Hello ");
-
-            for (String name : args) {
-                System.out.print(name + " ");
-            }
-=======
-<<<<<<< HEAD
             System.out.println("Hello " + String.join(" ", args));
-=======
-            String result = "Hello ";
-
-            for (String name : args) {
-                result += name + " ";
-            }
-
-            // remove last space
-            result = result.substring(0, result.length() - 1);
-
-            System.out.println(result);
->>>>>>> 5a276dfbd365bc1aad11e24052d36231107fde27
->>>>>>> 2a7cdcc103e7146f9d6448eb4b6b183df6506900
->>>>>>> 4da591df3a1cea06efeea898476888edb3f8c4c0
         }
 
     }
