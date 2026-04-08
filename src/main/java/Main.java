adccae233ce828f834dd98f15c2bcf6ae29fba62
AnilJ-beep <aniljampana12345@gmail.com>
2026-04-08 21:13:11 +0530

    UC2 implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
-	public static void main(String[] args) {
-        System.out.println("Hello App-UC1");
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+
     }
 }
